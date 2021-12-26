68906e6ca9becdd182304f1ca82647f6560b886b
Anas <alnabale2016@gmail.com>
2021-12-26 09:27:57 +0200

    create database tables

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -5,5 +5,31 @@ USE bookingSystem;
 
 CREATE TABLE sellers (
     Id INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255),
+    pass VARCHAR(255),
+    username VARCHAR(255),
+    filed VARCHAR(255),
+    summary VARCHAR(255),
     PRIMARY KEY (Id)
 );
+
+CREATE TABLE users (
+    Id INT AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255),
+    pass VARCHAR(255),
+    username VARCHAR(255),
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE appointment(
+    Id INT AUTO_INCREMENT NOT NULL,
+    phonenumber INT ,
+    app_date VARCHAR(255),
+    hour VARCHAR(255),
+    seller_name VARCHAR(255),
+    username VARCHAR(255),
+    app_status VARCHAR(255),
+    PRIMARY KEY (Id)
+
+
+); 
