206d5cb72624e275009909cade46fb14eab9b7d5
Anas <alnabale2016@gmail.com>
2021-12-27 12:33:43 +0200

    signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Sellers from "./components/sellers";
 import SignUp from "./components/auth/user_auth/signup";
 import UserAppointments from "./components/user_appointments";
 import SellerAppointments from "./components/seller_appointments";
+import Login from "./components/auth/user_auth/login";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Navigation />
             <Routes>
                 <Route exact path="/signup" element={<SignUp />} />
+                <Route exact path="/login" element={<Login />} />
                 <Route exact path="/my_appointments/:id" element={<UserAppointments />} />
                 <Route exact path="/clients_appointments/:id" element={<SellerAppointments />} />
                 <Route exact path="/" element={<Sellers />} />
