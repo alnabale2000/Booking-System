86e3f9e53e45faffc44f6cf67722478d6dca2f4e
Anas <alnabale2016@gmail.com>
2021-12-28 17:48:22 +0200

    footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
     return (
         <footer>
-            <p>hi</p>
+            <div className="so-icons">
+                <SocialIcon
+                    className="so-icon"
+                    url="https://www.facebook.com/profile.php?id=100016617975313"
+                    bgColor="white"
+                ></SocialIcon>
+                <SocialIcon
+                    className="so-icon"
+                    url="https://www.linkedin.com/in/anas-alnabale-0646421ba/"
+                    bgColor="white"
+                ></SocialIcon>
+                <SocialIcon
+                    className="so-icon"
+                    url="https://www.instagram.com/anas_alnabale18/"
+                    bgColor="white"
+                ></SocialIcon>
+            </div>
+            Or Contact me on :0790744031
+            <p class="copyright">Agents On Clouds © 2018</p>
         </footer>
     );
 };
